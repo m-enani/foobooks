7b13324d234daa5ec7db8a1ed01e1d9ebcba2103
Mahmoud Enani <m.a.enani21@gmail.com>
2017-03-20 19:51:47 -0400

    Practice controller setup, environments, packages

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,17 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+/**
+* Book related routes
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/books', 'BookController@index');
+Route::get('/books/{title?}', 'BookController@show');
+if(config('app.env') == 'local') {
+    Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+}
+/**
+* Practice
+*/
+Route::any('/practice/{n?}', 'PracticeController@index');
+/**
+* Main homepage visitors see when they visit just /
+*/
+Route::get('/', 'WelcomeController');
